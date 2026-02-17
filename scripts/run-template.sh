5b3420f83aa7782ec5e564e2484d3fbbb804ef6e
Jon Elsey <65968741+JonElsey@users.noreply.github.com>
2026-02-17 10:36:27 +0000

    Fixed run-template to use the actual template not the setup script

--- scripts/run-template.sh
+++ scripts/run-template.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 set -euo pipefail
-for YEAR in {2017..2024}; do
-    sed "s/YEAR_TO_FILL/${YEAR}/g" run-template.sh > $YEAR/run_${YEAR}.sh
-    chmod +x $YEAR/run_${YEAR}.sh
-done
+YEAR=YEAR_TO_FILL
+CONFIG=config_${YEAR}.yaml
+DATA_DIR="/embed2social-storage/${YEAR}"
+LOG="logs/${YEAR}.log"
+SCRIPTS="/scripts/embeddings/"
+mkdir -p logs
+nohup bash -c "
+    find \"${DATA_DIR}\" -maxdepth 1 -type f -name '*.tif' \
+      | python $SCRIPTS/embed_to_lsoa.py --config_path \"${CONFIG}\"
+" > "$LOG" 2>&1 
+
+echo "Started YEAR job in background (PID $!)"
+echo "Log is at $LOG"
